ef2353e01bfacb259b4c8e4c538fbccdab115bf2
Krasimir Nikolov <krasimir.nikolov@mtr-design.com>
2015-12-03 15:24:22 +0200

    Fixed code format

--- Extension.php
+++ Extension.php
@@ -8,60 +8,60 @@
 
 namespace Bearlikelion\TwigDebugBar;
 
-Class Extension extends \Twig_Extension
+class Extension extends \Twig_Extension
 {
   private $debugbar;
 
-	public function __construct()
-	{
-      $this->debugbar = new \DebugBar\StandardDebugBar();
-	    $this->renderer = $this->debugbar->getJavascriptRenderer();
+  public function __construct()
+  {
+    $this->debugbar = new \DebugBar\StandardDebugBar();
+    $this->renderer = $this->debugbar->getJavascriptRenderer();
 
-      $this->renderer->disableVendor('fontawesome');
-      $this->renderer->disableVendor('jquery');
-      $this->renderer->disableVendor('highlightjs');
+    $this->renderer->disableVendor('fontawesome');
+    $this->renderer->disableVendor('jquery');
+    $this->renderer->disableVendor('highlightjs');
 
-	    $pdo = new \DebugBar\DataCollector\PDO\TraceablePDO(\ORM::get_db());
-      $this->debugbar->addCollector(new \DebugBar\DataCollector\PDO\PDOCollector($pdo));
-	}
+    $pdo = new \DebugBar\DataCollector\PDO\TraceablePDO(\ORM::get_db());
+    $this->debugbar->addCollector(new \DebugBar\DataCollector\PDO\PDOCollector($pdo));
+  }
 
-	public function getFunctions()
-	{
-	    return array(
-	        'dbg_render' => new \Twig_Function_Method($this, 'render',  array('is_safe' => array('html'))),
-          'dbg_renderHead' => new \Twig_Function_Method($this, 'renderHead',  array('is_safe' => array('html'))),
-          'dbg_jsAssets' => new \Twig_Function_Method($this, 'dumpJsAssets',  array('is_safe' => array('html'))),
-          'dbg_cssAssets' => new \Twig_Function_Method($this, 'dumpCssAssets',  array('is_safe' => array('html')))
-	    );
-	}
+  public function getFunctions()
+  {
+    return array(
+      'dbg_render' => new \Twig_Function_Method($this, 'render',  array('is_safe' => array('html'))),
+      'dbg_renderHead' => new \Twig_Function_Method($this, 'renderHead',  array('is_safe' => array('html'))),
+      'dbg_jsAssets' => new \Twig_Function_Method($this, 'dumpJsAssets',  array('is_safe' => array('html'))),
+      'dbg_cssAssets' => new \Twig_Function_Method($this, 'dumpCssAssets',  array('is_safe' => array('html')))
+    );
+  }
 
-	public function render()
-	{
-		return $this->renderer->render();
-	}
+  public function render()
+  {
+  	return $this->renderer->render();
+  }
 
-	public function renderHead()
-	{
-		return $this->renderer->renderHead();
-	}
+  public function renderHead()
+  {
+  	return $this->renderer->renderHead();
+  }
 
     public function dumpJsAssets()
-	{
-	  return $this->renderer->dumpJsAssets();
-	}
+  {
+    return $this->renderer->dumpJsAssets();
+  }
 
-	public function dumpCssAssets()
-	{
-	  return $this->renderer->dumpCssAssets();
-	}
+  public function dumpCssAssets()
+  {
+    return $this->renderer->dumpCssAssets();
+  }
 
-	public function getDebugBar() {
-	  return $this->debugbar;
-	}
+  public function getDebugBar() {
+    return $this->debugbar;
+  }
 
-	public function getName()
-	{
-		return 'debugbar_extension';
-	}
+  public function getName()
+  {
+  	return 'debugbar_extension';
+  }
 }
 
